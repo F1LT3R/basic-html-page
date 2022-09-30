af8c2f2ab21f48042a344298d2a2fb22c3e0f3ae
Alistair MacDonald <F1LT3R@users.noreply.github.com>
2022-09-30 12:00:45 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello reuben!</h1>
+<div>
+  We use the same cool! as to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+console.log(456);
